7b73aacea175d86b0052cefe744ea526da28dce0
Darlington Kofa <darlington@lidonation.com>
2023-02-02 13:53:06 -0600

    make votes 2 decimals

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer.ts
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer.ts
@@ -34,19 +34,19 @@ createInertiaApp({
             .use(pinia);
 
         app.config.globalProperties.$filters = {
-            number(value, locale: string='en-US') {
+            number(value, digits = 0, locale: string='en-US') {
                 // Nine Zeroes for Billions
                 return Math.abs(Number(value)) >= 1.0e+9
 
-                    ? (Math.abs(Number(value)) / 1.0e+9).toFixed(0) + "B"
+                    ? (Math.abs(Number(value)) / 1.0e+9).toFixed(digits) + "B"
                     // Six Zeroes for Millions
                     : Math.abs(Number(value)) >= 1.0e+6
 
-                        ? (Math.abs(Number(value)) / 1.0e+6).toFixed(0) + "M"
+                        ? (Math.abs(Number(value)) / 1.0e+6).toFixed(digits) + "M"
                         // Three Zeroes for Thousands
                         : Math.abs(Number(value)) >= 1.0e+3
 
-                            ? (Math.abs(Number(value)) / 1.0e+3).toFixed(0) + "K"
+                            ? (Math.abs(Number(value)) / 1.0e+3).toFixed(digits) + "K"
 
                             : Math.abs(Number(value));
             },
