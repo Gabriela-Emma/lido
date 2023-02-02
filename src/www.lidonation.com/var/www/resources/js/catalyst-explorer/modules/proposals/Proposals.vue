3a59493fa0935dafca6f3aec29d1576723b6fa12
Darlington W <darlington@wlehfam.com>
2023-02-01 23:45:43 -0600

    added the ability to filter by user

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer/modules/proposals/Proposals.vue
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/modules/proposals/Proposals.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-3 max-w-7xl 2xl:max-w-full mx-auto">
+        class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-3 max-w-7xl 2xl:max-w-full mx-auto">
         <ProposalCard v-for="proposal in proposals" :proposal="proposal"></ProposalCard>
     </div>
 </template>
