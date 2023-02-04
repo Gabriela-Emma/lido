71f9544c2893e8d2f4e8015eca552b61c709a3dc
Darlington Kofa <darlington@lidonation.com>
2023-02-03 18:17:34 -0600

    adding follow proposal

--- src/www.lidonation.com/var/www/app/Http/Controllers/Api/CatalystExplorer/ReportController.php
+++ src/www.lidonation.com/var/www/app/Http/Controllers/Api/CatalystExplorer/ReportController.php
@@ -3,17 +3,63 @@
 namespace App\Http\Controllers\Api\CatalystExplorer;
 
 use App\Http\Controllers\Controller;
+use App\Models\CatalystReport;
+use App\Models\NotificationRequestTemplate;
+use App\Models\User;
 use \Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Fluent;
+use Illuminate\Support\Str;
 
 class ReportController extends Controller
 {
     public function follow(Request $request)
     {
-        $validated = $request->validate([
+        $validated = new Fluent($request->validate([
             'where' => 'email:rfc,dns',
-            'body' => 'required',
-        ]);
+            'name' => 'required',
+            'filter' => 'required',
+            'value' => 'required',
+        ]));
 
+        $nrt = new NotificationRequestTemplate;
+        $nrt->where = $validated->where;
+
+        // get authenticated user or create new one
+        $who = User::where('email', $validated->where)?->first();
+        if (!$who instanceof User) {
+            $who = new User;
+            $who->name = $validated->name;
+            $who->email = $validated->where;
+            $who->password = Hash::make(Str::random(30)) ?? null;
+            $who->save();
+        }
+        $nrt->who_id = $who->id;
+        $nrt->who_type = $who::class;
+
+        $nrt->what_type = CatalystReport::class;
+        $nrt->when = 'create';
+
+        $nrt->what_filter = [
+            'filterFunction' => 'whereRelation',
+            'operator' => '=',
+            'relation' => match ($validated->filter) {
+                'proposal' => 'proposal',
+                'group' => 'proposal.groups',
+                default => $validated->filter
+            },
+            'field' => match ($validated->filter) {
+                'proposal', 'group' => 'id',
+                default => $validated->filter
+            },
+            'value' => $validated->value,
+        ];
+
+        $nrt->status = 'published';
+
+        $nrt->save();
+
+        return $nrt;
     }
 
 }
