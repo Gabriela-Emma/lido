25accd53e8832489976103cca08f9deee38d3c37
tyty-SE <emmanuelshikuku370@gmail.com>
2023-02-07 18:06:08 +0300

    work in progress

---
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/modules/groups/Groups.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul role="list"
+        class="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 2xl:grid-cols-4 lg:gap-8">
+      <GroupCard v-for="group in groups" :catalystGroup="group"></GroupCard>             
+    </ul>
+</template>
+
+<script lang="ts" setup>
+import Group from '../../models/group';
+import GroupCard from './GroupCard.vue';
+
+const props=withDefaults(
+    defineProps<{
+        groups:Group[];
+    }>(),
+    {
+        groups:() => []
+    }
+)
+
+</script>
