3a2f48cb883482ef42acb39b589cf757b7af57bc
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-02-12 11:45:34 +0300

    clean up

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Shared/Components/HeaderComponent.vue
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Shared/Components/HeaderComponent.vue
@@ -36,7 +36,7 @@
                     </Link>
                 </li>
                 <li>
-                    <Link class="flex gap-1 items-center" href="/api/catalyst-explorer/logout" method="POST">
+                    <Link class="flex gap-1 items-center text-teal-600 font-bold hover:text-red-600" href="/api/catalyst-explorer/logout" method="POST" as="button">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="w-4 h-4 relative">
                             <path stroke-linecap="round" stroke-linejoin="round"
