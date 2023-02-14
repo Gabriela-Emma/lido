394c5d215fb177f08cea504300db038efa0aa237
Darlington Kofa <darlington@lidonation.com>
2023-02-14 11:56:21 -0600

    cleanup

--- src/www.lidonation.com/var/www/app/Nova/Actions/ValidateClaims.php
+++ src/www.lidonation.com/var/www/app/Nova/Actions/ValidateClaims.php
@@ -5,16 +5,13 @@ namespace App\Nova\Actions;
 use App\Models\Meta;
 use App\Models\User;
 use App\Enums\RoleEnum;
-use App\Models\CatalystUser;
 use Illuminate\Bus\Queueable;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\Hidden;
 use Laravel\Nova\Actions\Action;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Fields\ActionFields;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class ValidateClaims extends Action
@@ -24,21 +21,21 @@ class ValidateClaims extends Action
     /**
      * Perform the action on the given models.
      *
-     * @param  \Laravel\Nova\Fields\ActionFields  $fields
-     * @param  \Illuminate\Support\Collection  $models
-     * @return mixed
+     * @param ActionFields $fields
+     * @param Collection $models
+     * @return string[]
      */
-    public function handle(ActionFields $fields, Collection $models)
+    public function handle(ActionFields $fields, Collection $models): array
     {
         $code = $fields->code;
         foreach ($models as $model) {
             $id = $model->id;
             $meta = $model->metas()->where('key', 'ideascale_verification_code')->where('content', $code)->first();
-            // user making the claim 
+            // user making the claim
             $regularUser = User::whereHas('metas', function ($query) use ($code) {
                 $query->where('key', 'ideascale_verification_code')->where('content', $code);
             })->first();
-            
+
             // getting related data
             $data=Meta::where('key','claim_data')->where('model_id', $id)->first();
             $claim_data=collect(json_decode($data->content));
@@ -55,19 +52,21 @@ class ValidateClaims extends Action
             $userData = array_filter($dataUpdate, function ($value) {
                 return $value !== null && $value !== '';
             });
-                        
-            // validating claims 
+
+            // validating claims
             if ($meta && $regularUser) {
                 $regularUser->assignRole((string) RoleEnum::proposer());
                 $model->fill($userData);
                 $regularUser->catalyst_users()->save($model);
+                $regularUser->name = $claim_data['name'];
+                $regularUser->save();
+                
                 event(new Registered($model->user));
-
 
                 return Action::message('Validation performed successfully!');
             }
         }
-    
+
         return Action::danger('failed');
     }
 
