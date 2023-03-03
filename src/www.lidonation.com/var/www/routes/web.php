da6142163d6688833b770d597a589045013fdc82
Darlington Kofa <darlington@lidonation.com>
2023-03-02 20:20:01 -0600

    cleanup

--- src/www.lidonation.com/var/www/routes/web.php
+++ src/www.lidonation.com/var/www/routes/web.php
@@ -239,10 +239,14 @@ Route::group(
                 Route::get('/groups/{catalystGroup:id}/proposals', [CatalystMyGroupsController::class, 'proposals']);
                 Route::get('/groups/{catalystGroup:id}/manage', [CatalystMyGroupsController::class, 'manage']);
                 Route::delete('/groups/{catalystGroup:id}/proposals/{proposal:id}', [CatalystMyGroupsController::class, 'removeProposal']);
+
                 Route::post('/groups/{catalystGroup:id}/add', [CatalystMyGroupsController::class, 'addProposal']);
+
                 Route::get('/groups/{catalystGroup:id}/members', [CatalystMyGroupsController::class, 'getMembers']);
                 Route::delete('/groups/{catalystGroup:id}/remove/profiles/{member:id}', [CatalystMyGroupsController::class, 'removeMembers']);
+
                 Route::post('/groups/{catalystGroup:id}/add/members',[CatalystMyGroupsController::class, 'addMembers']);
+
                 Route::post('/groups', [CatalystGroupsController::class, 'create']);
                 Route::get('/groups', [CatalystMyGroupsController::class, 'index'])
                     ->name('myGroups');
