5a98787ab52084a3c142ebbf259632d3148ec3d9
Darlington Kofa <darlington@lidonation.com>
2023-03-06 14:42:32 -0600

    increase lido every epoch pool multiplier

--- src/www.lidonation.com/var/www/app/Invokables/GetPoolMultiplier.php
+++ src/www.lidonation.com/var/www/app/Invokables/GetPoolMultiplier.php
@@ -17,10 +17,14 @@ class GetPoolMultiplier
         $multiplier = 1.0;
         if ($account->pool_id === config('cardano.pool.hash')) {
             $multiplier = match (true) {
-                ($stake >= 100 * $lovelaces && $stake < (1000 * $lovelaces)) => 1.10,
-                ($stake >= 1000 * $lovelaces && $stake < (8000 * $lovelaces)) => 1.25,
-                ($stake >= 8000 * $lovelaces && $stake < (20000 * $lovelaces)) => 1.50,
-                ($stake >= 20000 * $lovelaces) => 1.80,
+                ($stake >= 100 * $lovelaces && $stake < (1000 * $lovelaces)) => 1.15,
+                ($stake >= 1000 * $lovelaces && $stake < (8000 * $lovelaces)) => 1.30,
+                ($stake >= 8000 * $lovelaces && $stake < (20000 * $lovelaces)) => 1.55,
+                ($stake >= 20000 * $lovelaces) => 1.85,
+                ($stake >= 30000 * $lovelaces) => 2,
+                ($stake >= 50000 * $lovelaces) => 2.25,
+                ($stake >= 75000 * $lovelaces) => 2.50,
+                ($stake >= 100000 * $lovelaces) => 3,
                 default => 1.0
             };
         }
