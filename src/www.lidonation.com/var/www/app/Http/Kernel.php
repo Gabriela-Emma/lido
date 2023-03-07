05c499feadcb352cd2c19cd8b2470b56b07db122
Darlington Kofa <darlington@lidonation.com>
2023-03-07 11:49:55 -0600

    adding ispector service

--- src/www.lidonation.com/var/www/app/Http/Kernel.php
+++ src/www.lidonation.com/var/www/app/Http/Kernel.php
@@ -42,12 +42,14 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             HandleInertiaRequests::class,
+            \Inspector\Laravel\Middleware\WebRequestMonitoring::class,
         ],
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Inspector\Laravel\Middleware\WebRequestMonitoring::class,
         ],
     ];
 
