83b4a401f36951828c615fe5e85f53802872ffe9
John-Nzioka <nziokajohn254@gmail.com>
2023-03-20 18:00:06 +0300

    Creating Question and Answer Seeder

--- src/www.lidonation.com/var/www/database/factories/QuestionFactory.php
+++ src/www.lidonation.com/var/www/database/factories/QuestionFactory.php
@@ -3,12 +3,16 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Question;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
  */
 class QuestionFactory extends Factory
 {
+    protected $model = Question::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,12 @@ class QuestionFactory extends Factory
     public function definition()
     {
         return [
-            //
-        ];
+            'user_id' => fn () => User::inRandomOrder()->first()->id,
+            'title' =>$this->faker->words(4, true),
+            'content'=>$this->faker->sentence(rand(2, 3), true),
+            'type' => $this->faker->randomElement(['general', 'technical', 'dev']),
+            'status' =>$this->faker->randomElement(['published', 'draft', 'published', 'pending', 'published']),
+            'created_at' => now(),
+            ];
     }
 }
--- src/www.lidonation.com/var/www/database/seeders/QuestionAnswerSeeder.php
+++ src/www.lidonation.com/var/www/database/seeders/QuestionAnswerSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\QuestionAnswer;
+use App\Models\Question;
 
 class QuestionAnswerSeeder extends Seeder
 {
@@ -13,6 +15,7 @@ class QuestionAnswerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        QuestionAnswer::factory(4)
+        ->create();
     }
 }
