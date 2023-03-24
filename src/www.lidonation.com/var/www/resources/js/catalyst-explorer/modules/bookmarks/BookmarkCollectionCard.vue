5f6c0a3ef28db89d89f75f771628a8fa7d7a7f74
Darlington Kofa <darlington@lidonation.com>
2023-03-24 15:02:58 -0500

    cleanup

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer/modules/bookmarks/BookmarkCollectionCard.vue
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/modules/bookmarks/BookmarkCollectionCard.vue
@@ -1,10 +1,12 @@
 <template>
     <a :href="collection?.link"
+       :class="[textColor$]"
        :style="{backgroundColor: collection?.color}"
        class="h-56 w-72 lg:max-w-xs p-3 object-cover shadow-md hover:shadow-xl rounded-l-xl rounded-r-xs flex flex-col justify-center relative">
         <div class="flex w-full justify-end absolute top-2 right-2">
             <button type="button"
-                    class="inline-flex items-center gap-x-0.5 rounded-sm border border-slate-800 py-1 px-1.5 hover:text-slate-400 text-xs font-semibold text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600">
+                    :class="[textColor$, borderColor$]"
+                    class="inline-flex items-center gap-x-0.5 rounded-sm border py-1 px-1.5 hover:text-slate-400 text-xs font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600">
                 {{ $t("View") }}
                 <ArrowTopRightOnSquareIcon class="-mr-0.5 h-3 w-3" aria-hidden="true"/>
             </button>
@@ -15,19 +17,19 @@
             </h2>
         </div>
         <div class="w-full flex gap-2 justify-end divide-x divide-slate-800" v-if="collection?.items_count > 0">
+<!--            <div-->
+<!--                class="inline-flex items-center items-center rounded-sm py-0.5 pl-2.5 pr-1 text-sm font-medium text-black">-->
+<!--                {{ $t("Items") }}-->
+<!--                <span-->
+<!--                    class="ml-0.5 inline-flex flex-shrink-0 items-center justify-center rounded-full text-black font-bold focus:outline-none">-->
+<!--                    {{ collection?.items_count }}-->
+<!--                </span>-->
+<!--            </div>-->
             <div
-                class="inline-flex items-center items-center rounded-sm py-0.5 pl-2.5 pr-1 text-sm font-medium text-black">
+                class="inline-flex items-center items-center rounded-sm py-0.5 pl-2.5 pr-1 text-sm font-medium">
                 {{ $t("Items") }}
                 <span
-                    class="ml-0.5 inline-flex flex-shrink-0 items-center justify-center rounded-full text-black font-bold focus:outline-none">
-                    {{ collection?.items_count }}
-                </span>
-            </div>
-            <div
-                class="inline-flex items-center items-center rounded-sm py-0.5 pl-2.5 pr-1 text-sm font-medium text-black">
-                {{ $t("Items") }}
-                <span
-                    class="ml-0.5 inline-flex flex-shrink-0 items-center justify-center rounded-full text-black font-bold focus:outline-none">
+                    class="ml-0.5 inline-flex flex-shrink-0 items-center justify-center rounded-full font-bold focus:outline-none">
                     {{ collection?.items_count }}
                 </span>
             </div>
@@ -36,7 +38,7 @@
 </template>
 
 <script lang="ts" setup>
-import {computed} from "vue";
+import {computed, inject} from "vue";
 import {usePage} from "@inertiajs/vue3";
 import User from "../../models/user";
 import BookmarkCollection from "../../models/bookmark-collection";
@@ -48,5 +50,13 @@ const props = withDefaults(
     }>(),
     {},
 );
+
+const $utils: any = inject('$utils');
 const user = computed(() => usePage().props?.user as User);
+const textColor$ = computed<string>(() =>
+    $utils?.contrastColor(props.collection?.color) === 'light' ? 'text-white' : 'text-black'
+);
+const borderColor$ = computed<string>(() =>
+    $utils?.contrastColor(props.collection?.color) === 'light' ? 'border-white' : 'border-black'
+);
 </script>
