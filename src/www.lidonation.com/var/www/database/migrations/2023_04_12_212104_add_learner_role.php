e92a1574be6a740bd51db665c1c84cc6fb32514e
michael_ochara <michaelteddy009@gmail.com>
2023-04-13 01:46:28 +0300

    implement register for JUT

---
+++ src/www.lidonation.com/var/www/database/migrations/2023_04_12_212104_add_learner_role.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Enums\RoleEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // dd(RoleEnum::learner()->values());
+        Role::create(['name' => 'learner']);
+    }
+};
--- src/www.lidonation.com/var/www/resources/js/earn/Pages/Register.vue
+++ src/www.lidonation.com/var/www/resources/js/earn/Pages/Register.vue
@@ -1,4 +1,35 @@
-<template></template>
-<script lang="ts" setup>
+<template>
+    <div class="bg-slate-100 login-form-wrapper">
+        <RegisterForm :showLogo="false" 
+                   :errors="errors" 
+                   @endpoint="setEndpoint($event)" 
+                   @setForm="getForm($event)" 
+                   @submit="submit"/>
+    </div>
+</template>
 
+<script lang="ts" setup>
+import { useForm } from "@inertiajs/vue3";
+import { ref } from "vue";
+import RegisterForm from "../Shared/Components/RegisterForm.vue";
+
+const props = withDefaults(
+    defineProps<{
+        errors?: Object,
+    }>(), {});
+
+let Endpoint= ref('')
+let form = useForm({})
+
+let setEndpoint = (url:string) => {
+    Endpoint.value = url
+}
+
+let getForm = (RegisterForm) => {
+    form = RegisterForm
+}
+
+let submit = () => {
+    form.post(Endpoint.value);
+}
 </script>
