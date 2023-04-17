98e388cf082bb408b7a9e05d2734a63d37105467
michael_ochara <michaelteddy009@gmail.com>
2023-04-17 18:13:27 +0300

    fix coloring and styling for quiz choices

--- src/www.lidonation.com/var/www/resources/js/earn/Pages/LearningLesson.vue
+++ src/www.lidonation.com/var/www/resources/js/earn/Pages/LearningLesson.vue
@@ -103,10 +103,10 @@
                                                                 v-model="selectedAnswer"/>
                                                         <div class="w-full rounded-md bg-white p-5" :class="correct == 'true' ? 'text-green-400' : 'text-orange-500'">
                                                             <div class="flex items-center justify-between">
-                                                            <p class="text-sm font-semibold">{{ answer.content }}</p>
-                                                            <div>
-                                                                <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z" /></svg>
-                                                            </div>
+                                                                <p class="text-sm font-semibold">{{ answer.content }}</p>
+                                                                <div class="w-4 h-4">
+                                                                    <svg fill='currentColor' id="Layer_1_1_" style="enable-background:new 0 0 16 16;" version="1.1" viewBox="0 0 16 16" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="8" cy="8" r="8"/></svg>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </label>
@@ -117,10 +117,10 @@
                                                                 :id="answer.id"/>
                                                         <div class="w-full rounded-md bg-white p-5 text-gray-500">
                                                             <div class="flex items-center justify-between">
-                                                            <p class="text-sm font-semibold">{{ answer.content }}</p>
-                                                            <div>
-                                                                <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z" /></svg>
-                                                            </div>
+                                                                <p class="text-sm font-semibold">{{ answer.content }}</p>
+                                                                <div class="w-4 h-4">
+                                                                    <svg fill='currentColor' id="Layer_1_1_" style="enable-background:new 0 0 16 16;" version="1.1" viewBox="0 0 16 16" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="8" cy="8" r="8"/></svg>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </label>
@@ -156,12 +156,12 @@
                                                                 name="answer" 
                                                                 :id="answer.id"
                                                                 @click="updateSelectedAnswer(answer.id)"/>
-                                                        <div class="w-full rounded-md bg-white text-gray-500 p-5 transition-all hover:shadow peer-checked:text-red-600">
+                                                        <div class="w-full rounded-md bg-white text-gray-500 p-5 transition-all hover:shadow peer-checked:text-labs-black">
                                                             <div class="flex items-center justify-between">
-                                                            <p class="text-sm font-semibold">{{ answer.content }}</p>
-                                                            <div>
-                                                                <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z" /></svg>
-                                                            </div>
+                                                                <p class="text-sm font-semibold">{{ answer.content }}</p>
+                                                                <div class="w-4 h-4">
+                                                                    <svg fill='currentColor' id="Layer_1_1_" style="enable-background:new 0 0 16 16;" version="1.1" viewBox="0 0 16 16" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="8" cy="8" r="8"/></svg>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </label>
