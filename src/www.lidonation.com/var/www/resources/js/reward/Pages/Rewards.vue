490cd9d5d81138047b25088ab36f40c75e64bbbd
Darlington Kofa <darlington@lidonation.com>
2023-04-17 11:50:40 -0500

    cleanup

--- src/www.lidonation.com/var/www/resources/js/reward/Pages/Rewards.vue
+++ src/www.lidonation.com/var/www/resources/js/reward/Pages/Rewards.vue
@@ -262,7 +262,7 @@ import LoginForm from '../../global/Shared/Components/LoginForm.vue'
 import {useWalletStore} from '../../catalyst-explorer/stores/wallet-store';
 import Wallet from '../../catalyst-explorer/models/wallet';
 import {storeToRefs} from 'pinia';
-import RewardData = App.DataTransferObjects.RewardsData
+import RewardData = App.DataTransferObjects.RewardData
 import {AxiosError} from 'axios';
 
 const ConnectWallet = defineAsyncComponent(() => import('../../global/Shared/Components/ConnectWallet.vue'));
