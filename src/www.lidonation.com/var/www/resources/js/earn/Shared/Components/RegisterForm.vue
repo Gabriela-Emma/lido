868fcfb6fff4d9658c57cf0aeed03ac3e4f7af26
michael_ochara <michaelteddy009@gmail.com>
2023-04-20 00:53:55 +0300

    LN-32 registration logic fix

--- src/www.lidonation.com/var/www/resources/js/earn/Shared/Components/RegisterForm.vue
+++ src/www.lidonation.com/var/www/resources/js/earn/Shared/Components/RegisterForm.vue
@@ -1,6 +1,81 @@
 <template>
     <div class="flex items-center justify-center h-screen bg-slate-100">
-        <form >
+        <form v-if="user">
+            <div class=" bg-white shadow-sm  rounded  p-6 w-96">
+                    <div class=" flex-col  mb-4 border-b">
+                        <div class="flex items-center justify-center mb-4">
+                            <img alt="catalyst explorer logo" :src="$utils.assetUrl('img/catalyst-explorer-logo.jpg')"
+                                class="h-40"/>
+                        </div>
+                    </div>
+                <div>
+                    <div class=" flex justify-start mb-2">
+                        <h1 class="text-2xl lg:text-3xl 2xl:text-5xl 3xl:text-6xl font-semibold text-slate-700"> {{ $t("Register") }} </h1>
+                    </div>
+
+                    <div v-if="!name" class="mb-2">
+                        <label for="name" class="block text-sm font-medium text-slate-600">{{ $t("Name") }} </label>
+                        <div class="mt-1">
+                            <input  v-model="name" v-text="form.errors.name" id="name" name="name" type="text" autocomplete="name" required
+                                class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                            <div v-if="form.errors.name" v-text="form.errors.name"
+                            class="text-red-500 text-xs mt-1"></div>
+                        </div>
+                    </div>
+                    <div v-if="email" class="mb-2">
+                        <label for="email" class="block text-sm font-medium text-slate-600">{{ $t("Email address") }} </label>
+                        <div class="mt-1">
+                            <input  v-model="email" id="email" name="email" type="email" autocomplete="email" required disabled
+                                class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                            <div v-if="form.errors.email" v-text="form.errors.email"
+                            class="text-red-500 text-xs mt-1"></div>
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <label for="wallet_address" class="block text-sm font-medium text-slate-600">{{ $t("ADA Wallet Address") }} </label>
+                        <div class="mt-1">
+                            <input  v-model="wallet_address" id="wallet_address" name="wallet_address" type="wallet_address" autocomplete="wallet_address" required
+                                class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                            <div v-if="form.errors.wallet_address" v-text="form.errors.wallet_address"
+                            class="text-red-500 text-xs mt-1"></div>
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <label for="twitter" class="block text-sm font-medium text-slate-600">{{ $t("Twitter") }} </label>
+                        <div class="mt-1">
+                            <input  v-model="twitter" id="twitter" name="twitter" type="twitter" autocomplete="twitter" placeholder="optional"
+                                class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                            <div v-if="form.errors.twitter" v-text="form.errors.twitter"
+                            class="text-red-500 text-xs mt-1"></div>
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <label for="telegram" class="block text-sm font-medium text-slate-600">{{ $t("Telegram") }} </label>
+                        <div class="mt-1">
+                            <input  v-model="telegram" id="telegram" name="telegram" type="telegram" autocomplete="telegram" placeholder="optional"
+                                class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                            <div v-if="form.errors.telegram" v-text="form.errors.telegram"
+                            class="text-red-500 text-xs mt-1"></div>
+                        </div>
+                    </div>
+
+                    <div class="mt-3 flex flex-col gap-2 ">
+                        <div class="">
+                            <button @click.prevent="submit" type="submit"
+                                class="flex gap-3 items-center w-full justify-center rounded-sm border border-transparent bg-teal-600 py-2 px-4 text-xl 2xl:text-2xl font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                                </svg>
+                            <span>{{ $t("Sign Up") }}</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+
+        <form v-else>
             <div class=" bg-white shadow-sm  rounded  p-6 w-96">
                     <div class=" flex-col  mb-4 border-b">
                         <div class="flex items-center justify-center mb-4">
@@ -16,7 +91,7 @@
                     <div class="mb-2">
                         <label for="name" class="block text-sm font-medium text-slate-600">{{ $t("Name") }} </label>
                         <div class="mt-1">
-                            <input  v-model="form.name" v-text="form.errors.name" id="name" name="name" type="text" autocomplete="name" required
+                            <input  v-model="name" v-text="form.errors.name" id="name" name="name" type="text" autocomplete="name" required
                                 class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
                             <div v-if="form.errors.name" v-text="form.errors.name"
                             class="text-red-500 text-xs mt-1"></div>
@@ -25,7 +100,7 @@
                     <div class="mb-2">
                         <label for="email" class="block text-sm font-medium text-slate-600">{{ $t("Email address") }} </label>
                         <div class="mt-1">
-                            <input  v-model="form.email" id="email" name="email" type="email" autocomplete="email" required
+                            <input  v-model="email" id="email" name="email" type="email" autocomplete="email" required
                                 class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
                             <div v-if="form.errors.email" v-text="form.errors.email"
                             class="text-red-500 text-xs mt-1"></div>
@@ -34,7 +109,7 @@
                     <div class="mb-2">
                         <label for="wallet_address" class="block text-sm font-medium text-slate-600">{{ $t("ADA Wallet Address") }} </label>
                         <div class="mt-1">
-                            <input  v-model="form.wallet_address" id="wallet_address" name="wallet_address" type="wallet_address" autocomplete="wallet_address" required
+                            <input  v-model="wallet_address" id="wallet_address" name="wallet_address" type="wallet_address" autocomplete="wallet_address" required
                                 class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
                             <div v-if="form.errors.wallet_address" v-text="form.errors.wallet_address"
                             class="text-red-500 text-xs mt-1"></div>
@@ -63,7 +138,7 @@
                     <div class="mb-2">
                         <label for="twitter" class="block text-sm font-medium text-slate-600">{{ $t("Twitter") }} </label>
                         <div class="mt-1">
-                            <input  v-model="form.twitter" id="twitter" name="twitter" type="twitter" autocomplete="twitter"
+                            <input  v-model="twitter" id="twitter" name="twitter" type="twitter" autocomplete="twitter" placeholder="optional"
                                 class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
                             <div v-if="form.errors.twitter" v-text="form.errors.twitter"
                             class="text-red-500 text-xs mt-1"></div>
@@ -72,7 +147,7 @@
                     <div class="mb-2">
                         <label for="telegram" class="block text-sm font-medium text-slate-600">{{ $t("Telegram") }} </label>
                         <div class="mt-1">
-                            <input  v-model="form.telegram" id="telegram" name="telegram" type="telegram" autocomplete="telegram"
+                            <input  v-model="telegram" id="telegram" name="telegram" type="telegram" autocomplete="telegram" placeholder="optional"
                                 class="block w-full appearance-none rounded-sm border border-slate-400 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
                             <div v-if="form.errors.telegram" v-text="form.errors.telegram"
                             class="text-red-500 text-xs mt-1"></div>
@@ -104,22 +179,51 @@
 </template>
 
 <script lang="ts" setup>
+import { storeToRefs } from "pinia";
+import {Ref, inject, computed, ref} from "vue";
 import {useForm, usePage} from '@inertiajs/vue3';
 import {Link} from '@inertiajs/vue3';
-import {inject} from "vue";
+import User from "../../..//global/Shared/Models/user";
+import { useWalletStore } from "../../../catalyst-explorer/stores/wallet-store";
+import Wallet from '../../../catalyst-explorer/models/wallet';
+
 const $utils: any = inject('$utils');
+const user = computed(() => usePage().props.user as User)
 
 const props=defineProps({
     errors:Object
 })
+
+
+let name = ref(null);
+let email = ref(null);
+let wallet_address = ref(null);
+let wallet_stake_address = ref(null);
+let twitter = ref(null);
+let telegram = ref(null);
+
+let walletStore = useWalletStore();
+let {walletData} = storeToRefs(walletStore);
+let myWallet:Ref<Wallet> = computed(() => walletData.value);
+
+if (user) {
+    name.value = user.value?.name;
+    email.value = user.value?.email;
+    wallet_address.value = myWallet.value?.address;
+    wallet_stake_address.value = myWallet.value?.stakeAddress;
+    twitter.value = user.value?.twitter;
+    telegram.value = user.value?.telegram;
+}
+
 let form= useForm({
-    name:'',
-    email:'',
+    name: name,
+    email:email,
     password:'',
     password_confirmation:'',
-    wallet_address:'',
-    twitter:'',
-    telegram:''
+    wallet_address: wallet_address.value  ?? wallet_address,
+    wallet_stake_address: wallet_stake_address.value,
+    twitter: twitter,
+    telegram: telegram
 });
 
 const baseUrl = usePage().props.base_url;
