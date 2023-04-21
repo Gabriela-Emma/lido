16f98a441babf57dc079328ff3deb3a263d09d85
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-04-21 17:38:44 +0300

    LN-48 Translation of the how to stake Cardano ADA ada to sw

---
+++ src/www.lidonation.com/var/www/database/migrations/2023_04_18_140952_add_how_to_stake_your_ada_snippets.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $json = File::get(base_path('database/files/how-to-stake-your-ada-snippets.json'));
+        $data = json_decode($json);
+
+        foreach ($data->snippets as $snippet) {
+            DB::table('snippets')->insert([
+                'user_id' => 1,
+                'name' => $snippet->heading,
+                'content' => json_encode(['en' => $snippet->text_en, 'sw' => $snippet->text_sw,'es' => '',  'fr' => '', 'zh' => '', 'ja' => '']),
+                'context' => 'global',
+                'type' => 'App\Models\Snippet',
+                'order' => 0,
+                'status' => 'published',
+                'created_at' => date('Y-m-d', time()),
+                'updated_at' => date('Y-m-d', time()),
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('snippets', function (Blueprint $table) {
+            //
+        });
+    }
+};
