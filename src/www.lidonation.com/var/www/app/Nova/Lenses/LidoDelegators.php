8f567a717219e128f85b7327a4b37d6148051f68
michael_ochara <michaelteddy009@gmail.com>
2023-04-29 23:05:38 +0300

    LN-80 refactoring lido delegators nova lens

---
+++ src/www.lidonation.com/var/www/app/Nova/Lenses/LidoDelegators.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Nova\Lenses;
+
+use App\Services\CardanoBlockfrostService;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\LensRequest;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Lenses\Lens;
+
+class LidoDelegators extends Lens
+{
+    /**
+     * Get the query builder / paginator for the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\LensRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return mixed
+     */
+    public static function query(LensRequest $request, $query)
+    {
+        $poolId = config('cardano.pool.hash');
+        $delegatorsAddresses = app(CardanoBlockfrostService::class)->get('pools/'.$poolId.'/delegators', null)->collect()->pluck('address');
+    
+        return $request->withOrdering($request->withFilters(
+            $query->whereIn('wallet_stake_address', $delegatorsAddresses)
+        ));
+    }
+
+    /**
+     * Get the fields available to the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Text::make('Name')
+                ->sortable(),
+
+            Text::make('Email')
+                ->sortable(),
+        ];
+    }
+
+    /**
+     * Get the cards available on the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available on the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return parent::actions($request);
+    }
+
+    /**
+     * Get the URI key for the lens.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'lido-delegators';
+    }
+}
