ec7203cd09f7a4fd2bb0783159a054e18c355cca
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-05-01 15:52:01 +0300

    LN-86 Add the delegation partition metric for the user resource

---
+++ src/www.lidonation.com/var/www/app/Nova/Metrics/Delegation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use App\Models\User;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Metrics\Partition;
+
+class Delegation extends Partition
+{
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return mixed
+     */
+    public function calculate(NovaRequest $request)
+    {
+        $lidoPoolId = config('cardano.pool.hash');
+
+        $lidoCount = User::where('active_pool_id', '=', $lidoPoolId)->count();
+        $otherPoolsCount = User::where('active_pool_id', '!=', $lidoPoolId)
+                                ->whereNotNull('active_pool_id')
+                                ->count();
+        $undelegatedCount = User::whereNull('active_pool_id')->count();
+        return $this->result([
+            'Lido' => $lidoCount,
+            'Other pools' => $otherPoolsCount,
+            'Undelegated' => $undelegatedCount,
+        ]);
+    }
+
+    /**
+     * Determine the amount of time the results of the metric should be cached.
+     *
+     * @return \DateTimeInterface|\DateInterval|float|int|null
+     */
+    public function cacheFor()
+    {
+        // return now()->addMinutes(5);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'delegation';
+    }
+}
