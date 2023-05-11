a4fa4b9317708e97ecd9a04be5e4bc79014e355f
Emmanuel-SE <emmanueltiti370@gmail.com>
2023-05-11 21:27:47 +0300

    LN-104 extending  global-search to lidosearch

--- src/www.lidonation.com/var/www/resources/views/components/public/continue-reading.blade.php
+++ src/www.lidonation.com/var/www/resources/views/components/public/continue-reading.blade.php
@@ -4,12 +4,11 @@
     'theme' => 'gray',
     'linkClasses' => '',
     'text' => $snippets->continueReading,
-    'query'=> ''
     ])
-<span {{$attributes->merge(['class' => 'my-2 mt-auto pt-4 inline-block'])}}>
+<span {{$attributes->merge(['class' => 'my-2 mt-auto pt-4 inline-block'])}} >
     @switch($style)
         @case('button')
-        <a href="{{strlen($query)>0 ? route($route,$query) : $link ?? route($route)}}" type="button"
+        <a href="{{$link ?? route($route)}}" type="button"
             class="flex w-full items-center px-6 py-3 border border-{{$theme}}-300 font-medium rounded-sm text-gray-700 bg-white hover:bg-{{$theme}}-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 {{$linkClasses}}">
                 <span>{{$text}}</span>
                 <span class="ml-3">
@@ -22,7 +21,7 @@
             </a>
         @break
         @default
-        <a href="{{strlen($query)>0 ? route($route,$query) : $link ?? route($route)}}" class="flex flex-row text-{{$theme}}-600 items-center w-full">
+        <a href="{{$link ?? route($route)}}" class="flex flex-row text-{{$theme}}-600 items-center w-full">
                 <span>{{$text}}</span>
                 <span class="ml-3">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
