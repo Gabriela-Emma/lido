14114f40f6a3b05483b2dcc52addea364230cdae
Emmanuel-SE <emmanueltiti370@gmail.com>
2023-05-12 13:21:19 +0300

    LN-104 search app

--- src/www.lidonation.com/var/www/resources/views/components/public/menu-primary.blade.php
+++ src/www.lidonation.com/var/www/resources/views/components/public/menu-primary.blade.php
@@ -5,8 +5,8 @@
         </span>
     </a>
 </li>
-<li class="flex items-center flow-root h-full menu-item global-search" x-data>
-    <a href="#" class="inline-flex items-center flex-1 menu-link group"
+<li id="search" class="flex items-center flow-root h-full menu-item global-search" x-data>
+    <a  href="#" class="inline-flex items-center flex-1 menu-link group"
        @click.prevent="$dispatch('open-global-search')">
         <span class="inline-flex items-center px-2.5 py-1 text-xs font-semibold text-white rounded-sm border border-white shadow-xs group-hover:bg-accent-600 group-focus:outline-none group-focus:ring-2 group-focus:ring-offset-2 group-focus:ring-gray-accent search-btn">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
