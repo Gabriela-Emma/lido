3041d869888e40a9f79218ad93a9c69ac5478ba5
Darlington Kofa <darlington@lidonation.com>
2023-05-17 11:35:03 -0500

    disable auto google translation

--- src/www.lidonation.com/var/www/resources/views/layouts/earn.blade.php
+++ src/www.lidonation.com/var/www/resources/views/layouts/earn.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" translate="no">
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <meta name="google" content="notranslate" />
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
