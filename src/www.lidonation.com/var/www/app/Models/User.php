79b0ec6e86d6d5823565892478e40d767980a3da
Darlington Kofa <darlington@lidonation.com>
2023-06-18 12:09:00 -0500

    resolve cannot mass edit user resources using the EditModel action

--- src/www.lidonation.com/var/www/app/Models/User.php
+++ src/www.lidonation.com/var/www/app/Models/User.php
@@ -63,7 +63,7 @@ class User extends Authenticatable implements HasMedia, Interfaces\IHasMetaData,
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'lang'
     ];
 
     /**
