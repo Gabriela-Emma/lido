3c64a510bfec9eebae8c5fdce6826ca35f6edf31
Darlington Kofa <darlington@lidonation.com>
2023-06-21 13:14:13 -0500

    resolve currency broken on funds page

--- src/www.lidonation.com/var/www/resources/js/lib/utils/currency.ts
+++ src/www.lidonation.com/var/www/resources/js/lib/utils/currency.ts
@@ -1,10 +1,12 @@
+import { shortNumber } from "./shortNumber";
+
 export function currency(value, currency: string = 'USD', locale: string = 'en-US',  maximumFractionDigits = 0) {
     if (typeof value !== "number") {
         return value;
     }
     switch (currency) {
         case 'ADA':
-            return `${value} ₳`;
+            return shortNumber(value, 2) + ' ₳';
         default:
             const formatter = new Intl.NumberFormat(locale, {
                 style: 'currency',
