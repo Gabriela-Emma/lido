7fd522ed93c7c1deecd199a437b8560488e88a3e
Emmanuel-SE <emmanueltiti370@gmail.com>
2023-06-26 10:49:07 +0300

    LN-175 cleanup

--- src/www.lidonation.com/var/www/resources/js/global/Shared/Components/LoginForm.vue
+++ src/www.lidonation.com/var/www/resources/js/global/Shared/Components/LoginForm.vue
@@ -1,15 +1,14 @@
 <template>
     <div :class="{
         'flex items-center justify-center h-screen': !embedded,
-        'bg-slate-100':forRewards === false
+        'bg-slate-100': forRewards === false
     }" class="login-form-wrapper">
         <form>
-            <div class="p-6 rounded  w-96"
-                 :class="{'bg-white shadow-sm ':forRewards === false}">
+            <div class="p-6 rounded w-[467px]" :class="{ 'bg-white shadow-sm ': forRewards === false }">
                 <div class="flex-col mb-4 border-b " v-if="showLogo">
                     <div class="flex items-center justify-center mb-4">
                         <img alt="catalyst explorer logo" :src="$utils.assetUrl('img/catalyst-explorer-logo.jpg')"
-                             class="h-40"/>
+                            class="h-40" />
                     </div>
                 </div>
 
@@ -21,14 +20,13 @@
 
                     <div class="mb-2">
                         <label for="email" class="block text-sm font-medium text-slate-700">{{
-                                $t("Email address")
-                            }}</label>
+                            $t("Email address")
+                        }}</label>
                         <div class="mt-1 ">
-                            <input v-model="form.email" id="email" name="email" type="email" autocomplete="email"
-                                   required
-                                   class="block w-full px-3 py-2 border rounded-sm shadow-sm appearance-none border-slate-400 placeholder-slate-400 focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
-                            <div v-if="form.errors.email" v-text="form.errors.email"
-                                 class="mt-1 text-xs text-red-500"></div>
+                            <input v-model="form.email" id="email" name="email" type="email" autocomplete="email" required
+                                class="block w-full px-3 py-2 border rounded-sm shadow-sm appearance-none border-slate-400 placeholder-slate-400 focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                            <div v-if="form.errors.email" v-text="form.errors.email" class="mt-1 text-xs text-red-500">
+                            </div>
                         </div>
                     </div>
 
@@ -36,59 +34,51 @@
                         <label for="email" class="block text-sm font-medium text-slate-700">{{ $t("Password") }}</label>
                         <div class="mt-1">
                             <input v-model="form.password" id="password" name="password" type="password"
-                                   autocomplete="password" required
-                                   class="block w-full px-3 py-2 border rounded-sm shadow-sm appearance-none border-slate-400 placeholder-slate-400 focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
+                                autocomplete="password" required
+                                class="block w-full px-3 py-2 border rounded-sm shadow-sm appearance-none border-slate-400 placeholder-slate-400 focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm">
                             <div v-if="form.errors.password" v-text="form.errors.password"
-                                 class="mt-1 text-xs text-red-500"></div>
+                                class="mt-1 text-xs text-red-500"></div>
                         </div>
                     </div>
 
                     <div class="flex items-center justify-between mt-4 mb-2">
                         <div class="flex items-center">
                             <input v-model="form.remember" id="remember-me" name="remember-me" type="checkbox"
-                                   class="w-4 h-4 text-teal-600 rounded border-slate-300 focus:ring-teal-500">
+                                class="w-4 h-4 text-teal-600 rounded border-slate-300 focus:ring-teal-500">
                             <label for="remember" class="block ml-2 text-sm text-slate-900">{{
-                                    $t("Remember me")
-                                }}</label>
+                                $t("Remember me")
+                            }}</label>
                         </div>
 
                         <div class="text-sm">
-                            <a href="/forgot-password"
-                               class="font-medium text-teal-300 hover:text-teal-100">
+                            <a href="/forgot-password" class="font-medium text-teal-300 hover:text-teal-100">
                                 {{ $t("Forgot your password") }}?
                             </a>
                         </div>
                     </div>
 
                     <div class="flex items-end justify-between ">
-                        <button @click.prevent="submit"
-                                type="submit"
-                                class="flex items-center justify-center w-full gap-3 px-4 py-2 text-xl font-medium text-white bg-teal-600 border border-transparent rounded-sm shadow-sm 2xl:text-2xl hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2">
+                        <button @click.prevent="submit" type="submit"
+                            class="flex items-center justify-center w-full gap-3 px-4 py-2 text-xl font-medium text-white bg-teal-600 border border-transparent rounded-sm shadow-sm 2xl:text-2xl hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" class="w-7 h-7">
+                                stroke="currentColor" class="w-7 h-7">
                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"/>
+                                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
                             </svg>
                             <span>{{ $t("Sign in") }}</span>
                         </button>
                         <span class="text-sm">
                             <button type="button" @click.prevent="emit('go-to-register')"
-                                  class="font-bold text-teal-600 hover:text-teal-500"
-                                  preserve-scroll>{{ $t("Register") }}</button>
+                                class="font-bold text-teal-600 hover:text-teal-500" preserve-scroll>{{ $t("Register")
+                                }}</button>
                         </span>
                     </div>
                 </div>
-                <Divider v-show="showDivider"/>
+                <Divider v-show="showDivider" />
                 <div v-show="showWalletBtn" class="flex flex-col items-center">
-                    <div v-if="walletError" v-text="walletError"
-                         class="my-1 text-sm text-red-500"></div>
+                    <div v-if="walletError" v-text="walletError" class="my-1 text-sm text-red-500"></div>
                     <WalletLoginBtn @walletLoginSuccessful="emit('success', $event)"
-                                    @walletError="handleWalletError($event)" />
-                    <DisconnectWalletBtn v-if="walletName" class="my-1">
-                        <button type="button" class="text-sm text-slate-800 hover:text-slate-800">
-                            {{ $t("Disconnect") }} {{ walletName }}
-                        </button>
-                    </DisconnectWalletBtn>
+                        @walletError="handleWalletError($event)" />
                 </div>
             </div>
         </form>
@@ -96,13 +86,13 @@
 </template>
 
 <script lang="ts" setup>
-import {useForm} from '@inertiajs/vue3';
-import {Link} from '@inertiajs/vue3';
-import {inject, Ref, ref} from "vue";
+import { useForm } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
+import { inject, Ref, ref } from "vue";
 import WalletLoginBtn from './WalletLoginBtn.vue';
 import DisconnectWalletBtn from './DisconnetWalletBtn.vue'
 import { storeToRefs } from 'pinia';
-import {useWalletStore} from "../../../catalyst-explorer/stores/wallet-store";
+import { useWalletStore } from "../../../catalyst-explorer/stores/wallet-store";
 import Divider from './Divider.vue';
 import User from "../Models/user";
 
@@ -133,7 +123,7 @@ let form = useForm({
 })
 
 let walletStore = useWalletStore();
-let {walletName} = storeToRefs(walletStore);
+let { walletName } = storeToRefs(walletStore);
 
 let walletError = ref('');
 let handleWalletError = (error) => {
