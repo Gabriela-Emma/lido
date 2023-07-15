c48f4d681578c7183204ca279dfc4322b91191f8
Darlington Kofa <darlington@lidonation.com>
2023-07-15 10:07:41 -0500

    LN-182 working on draft ballot

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Pages/EditDraftBallot.vue
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Pages/EditDraftBallot.vue
@@ -14,8 +14,8 @@
                         <div class="col-span-4">
                             <ul role="list" class="divide-y divide-gray-200">
                                 <li class="ml-4" v-for="item in group.items" :key="item.id">
-                                    <div class="flex justify-start gap-0 hover:bg-gray-50">
-                                        <div class="flex flex-col flex-none w-16 gap-2 px-2 py-4 bg-slate-200">
+                                    <div class="flex justify-start gap-0 px-4 py-4 hover:bg-gray-50">
+                                        <div class="flex flex-col flex-none w-16 gap-2 px-1 py-2 bg-slate-100">
                                             <div class="flex gap-2 flex-nowrap">
                                                 <div class="flex-1 w-1/2">
                                                     <HandThumbUpIcon aria-hidden="true" class="w-6 h-6 text-gray-500 hover:text-teal-600 hover:cursor-pointer" />
@@ -29,7 +29,7 @@
                                                 class="w-5 h-5 text-gray-500 hover:text-teal-600 hover:cursor-pointer" />
                                             </div>
                                         </div>
-                                        <div class="flex items-center flex-1 px-4 py-4 sm:px-6">
+                                        <div class="flex items-center flex-1 sm:px-6">
                                             <div class="flex-1 min-w-0 sm:flex sm:items-center sm:justify-between">
                                                 <div class="truncate">
                                                     <div class="flex flex-col text-md">
