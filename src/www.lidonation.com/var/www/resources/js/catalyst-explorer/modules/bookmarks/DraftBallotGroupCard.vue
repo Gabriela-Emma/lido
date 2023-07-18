f2d9364355ffc0b4717f01e7e0a4979c4a5a9871
Darlington Kofa <darlington@lidonation.com>
2023-07-18 15:46:00 -0500

    bug fixes

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer/modules/bookmarks/DraftBallotGroupCard.vue
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/modules/bookmarks/DraftBallotGroupCard.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <div class="px-5">
-        <div class="flex gap-3 mb-4">
+        <div class="flex flex-wrap gap-3 mb-4">
             <h2 class="text-sm md:text-md lg:text-2xl">{{ group.title }}</h2>
             <div class="flex items-center divide-x space-x-3 divide-gray-200 gap-1.5">
                 <div>
