c7625296c4d2327c5dc8dde55f9a505151becc10
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2023-07-18 13:03:33 +0300

    Fix the user store after login and logout

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Shared/Components/HeaderComponent.vue
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Shared/Components/HeaderComponent.vue
@@ -34,15 +34,14 @@
                                 </Link>
                             </li>
                             <li>
-                                <Link @onSuccess="logout()" class="flex items-center gap-1 font-bold text-teal-600 hover:text-red-600"
-                                    :href="route('catalystExplorerApi.logout')" method="post" as="button">
+                                <button @click.prevent="logout()" class="flex items-center gap-1 font-bold text-teal-600 hover:text-red-600" as="button">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                         stroke="currentColor" class="relative w-4 h-4">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"/>
                                     </svg>
                                     <span>{{ $t('logout') }}</span>
-                                </Link>
+                                </button>
                             </li>
                         </ul>
                     </div>
