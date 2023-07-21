937e68f44376c012df9b41a312dccd2a4ef07dd3
Darlington Kofa <darlington@lidonation.com>
2023-07-21 13:38:41 -0500

    bug fixes

--- src/www.lidonation.com/var/www/app/Observers/ProposalObserver.php
+++ src/www.lidonation.com/var/www/app/Observers/ProposalObserver.php
@@ -3,9 +3,7 @@
 namespace App\Observers;
 
 use App\Invokables\FillPostData;
-use App\Models\Model;
 use App\Models\Proposal;
-use App\Models\User;
 
 class ProposalObserver extends CatalystObserver
 {
@@ -20,12 +18,5 @@ class ProposalObserver extends CatalystObserver
         (new FillPostData)($proposal, [], fn () => [
             'type' => ['type', 'proposal'],
         ]);
-    }
-
-    public function deleting(Model|User $model)
-    {
-        if ($model->isForceDeleting()) {
-            parent::deleting($model);
-        }
     }
 }
