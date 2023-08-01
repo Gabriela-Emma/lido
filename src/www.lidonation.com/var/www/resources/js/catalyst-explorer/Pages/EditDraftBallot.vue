e2275dbc7495c040cab9c085111a12784ac2b5a6
jnzioka <nziokajohn254@gmail.com>
2023-07-31 21:34:56 -0400

    LN-237 draft ballot improved

--- src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Pages/EditDraftBallot.vue
+++ src/www.lidonation.com/var/www/resources/js/catalyst-explorer/Pages/EditDraftBallot.vue
@@ -1,6 +1,22 @@
 <template>
     <header-component titleName0="Draft Ballot" :titleName1="draftBallot$?.title"
-        :subTitle="`Created ${$filters.timeAgo(draftBallot$.created_at)}. Has ${draftBallot$?.items_count} item${draftBallot$?.items_count > 1 ? 's' : ''}.`" />
+        :subTitle="`Created ${$filters.timeAgo(draftBallot$.created_at)}. Has ${draftBallot$?.items_count} item${draftBallot$?.items_count > 1 ? 's' : ''}.`">
+
+        <div class="flex gap-1 divide-x space-x-3 flex-row bg-gray-50">
+            <div class="flex flex-rowflex-1 w-1/2">
+                <span class="mr-1">total: </span>
+                <span>{{ totalAllUpvotes + totalAllDownvotes }}</span>
+            </div>
+            <div class="flex flex-row flex-1 w-1/2">
+                <HandThumbUpIcon aria-hidden="true" class="w-8 h-8 text-gray-500 mr-1" />
+                <span>{{ totalAllUpvotes }}</span>
+            </div>
+            <div class="flex flex-row flex-1 w-1/2">
+                <HandThumbDownIcon aria-hidden="true" class="w-8 h-8 text-gray-500 mr-1" />
+                <span> {{ totalAllDownvotes }}</span>
+            </div>
+        </div>
+    </header-component>
 
     <main class="flex flex-col gap-2 py-8 bg-primary-20">
         <div class="container">
@@ -35,13 +51,13 @@
             <section class="">
                 <div class="flex w-full items-center justify-end space-x-0.5 mb-3 gap-2">
                     <a :href="draftBallot?.link" target="_blank">
-                        <button  class="bg-white rounded-sm px-3 py-2.5 text-gray-400 flex-wrap hover:text-yellow-500">
+                        <button class="bg-white rounded-sm px-3 py-2.5 text-gray-400 flex-wrap hover:text-yellow-500">
                             Share
                         </button>
                     </a>
                     <Link :href="route('catalystExplorer.draftBallotupdate.view', { draftBallot: draftBallot?.hash })"
                         class="bg-white rounded-sm px-3 py-2.5 text-gray-400 flex-wrap hover:text-yellow-500">
-                        Edit
+                    Edit
                     </Link>
                 </div>
                 <DraftBallotGroupCard v-for="group in draftBallot$.groups" :key="group.id" :group="group"
@@ -55,7 +71,7 @@
 import DraftBallot from '../models/draft-ballot';
 import DraftBallotGroupCard from '../modules/bookmarks/DraftBallotGroupCard.vue';
 import Search from '../../earn/Shared/Components/Search.vue';
-import { Ref, ref, watch } from "vue";
+import { Ref, computed, ref, watch } from "vue";
 import axios from 'axios';
 import { useBookmarksStore } from "../stores/bookmarks-store";
 import { storeToRefs } from 'pinia';
@@ -64,6 +80,9 @@ import Proposal from '../models/proposal';
 import { useUserStore } from '../../global/Shared/store/user-store';
 import route from 'ziggy-js';
 import { Link } from '@inertiajs/vue3';
+import { VOTEACTIONS } from '../models/vote-actions';
+import { HandThumbUpIcon, HandThumbDownIcon } from '@heroicons/vue/20/solid';
+
 
 const userStore = useUserStore();
 const { user$ } = storeToRefs(userStore);
@@ -106,8 +125,7 @@ watch([onLocal, inLastTenMins], () => {
     canDelete.value = (onLocal.value && inLastTenMins.value) || user$.value?.id === draftBallot$.value?.user_id;
 });
 
-function searchProposals()
-{
+function searchProposals() {
     const params = {
         search: search.value,
         fund_id: 113,
@@ -124,5 +142,22 @@ function searchProposals()
 async function bookmarkProposal(proposal: Proposal) {
     bookmarksStore.bookmarkProposal(proposal);
 }
+
+
+const totalAllUpvotes = computed(() => {
+    return draftBallot$.value.groups?.reduce((acc, group) => {
+        return acc + group.items?.reduce((acc, item) => {
+            return acc + (item.model?.vote?.vote === VOTEACTIONS.UPVOTE ? 1 : 0);
+        }, 0) || 0;
+    }, 0) || 0;
+});
+
+const totalAllDownvotes = computed(() => {
+    return draftBallot$.value.groups?.reduce((acc, group) => {
+        return acc + group.items?.reduce((acc, item) => {
+            return acc + (item.model?.vote?.vote === VOTEACTIONS.DOWNVOTE ? 1 : 0);
+        }, 0) || 0;
+    }, 0) || 0;
+});
 
 </script>
