8e0640ad4b06dbcc68ad2333d981d7901c8deba9
Emmanuel-SE <emmanueltiti370@gmail.com>
2023-08-06 09:53:43 +0300

    wip

--- src/www.lidonation.com/var/www/resources/js/global/Shared/Components/GlobalPlayer.vue
+++ src/www.lidonation.com/var/www/resources/js/global/Shared/Components/GlobalPlayer.vue
@@ -69,12 +69,13 @@
                                 class="inline-flex items-center hover:cursor-pointer hover:text-yellow-500 rounded-md bg-slate-800 px-1.5 py-0.5 text-xs font-medium text-slate-100">1x</span>
                         </div>
                         <div class="inline-flex items-center hover:cursor-pointer hover:text-slate-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6" @click="mute">
                                 <path
                                     d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 001.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06zM18.584 5.106a.75.75 0 011.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 11-1.06-1.06 8.25 8.25 0 000-11.668.75.75 0 010-1.06z" />
                                 <path
                                     d="M15.932 7.757a.75.75 0 011.061 0 6 6 0 010 8.486.75.75 0 01-1.06-1.061 4.5 4.5 0 000-6.364.75.75 0 010-1.06z" />
                             </svg>
+                                <input id="volume-slider" type="range" min="0" :max="1" step="0.01"  v-model="volume" @change="changeVolume">
                         </div>
                     </div>
                 </div>
@@ -92,7 +93,7 @@
                 'bottom-0 md:-bottom-60': 1,
                 '-bottom-60': !1,
             }">
-                <div class="p-2 bg-yellow-500 border-t border-l border-r border-yellow-600 rounded-t-sm w-80" v-if="1">
+                <div class="hidden p-2 bg-yellow-500 border-t border-l border-r border-yellow-600 rounded-t-sm w-80">
                     <div class="embed-wrapper">
                         <div>
                             <div class="plyr__video-embed" id="player">
@@ -120,9 +121,9 @@ const player = ref(null);
 let currentTime = ref(0);
 let duration = ref(0);
 let volume = ref(10);
-let padTo2Digits = (num) => {
-    num.toString().padStart(2, '0');
-}
+
+
+
 const formatTime = (time) => new Date(time * 1000).toISOString().substr(14, 5);
 const currentTimeFormatted = computed(() => formatTime(currentTime.value));
 const durationFormatted = computed(() => formatTime(duration.value));
@@ -177,5 +178,10 @@ function toggle() {
     } else {
         player.value.play();
     }
+}
+
+function mute() {
+    
+    
 }
 </script>
