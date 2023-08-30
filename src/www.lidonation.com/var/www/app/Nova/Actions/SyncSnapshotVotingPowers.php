d9f185bae6b2b5d37043abab7c6b2f177c37140a
Darlington Kofa <darlington@lidonation.com>
2023-08-30 02:40:10 -0500

    deploying kupo

---
+++ src/www.lidonation.com/var/www/app/Nova/Actions/SyncSnapshotVotingPowers.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use App\Jobs\SyncCatalystVotingPowersJob;
+use App\Models\CatalystRegistration;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class SyncSnapshotVotingPowers extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * The displayable name of the action.
+     *
+     * @var string
+     */
+    public $name = 'Sync Snapshot Voting Powers';
+
+     /**
+     * The text to be used for the action's confirm button.
+     *
+     * @var string
+     */
+    public $confirmButtonText = 'Sync';
+
+    /**
+     * Perform the action on the given models.
+     */
+    public function handle(ActionFields $fields, Collection $models): mixed
+    {
+        $models->each(function ($snapshot) {
+            try {
+                $registeredStakePub = CatalystRegistration::where('created_at', '<', $snapshot->snapshot_at)
+                    ->pluck('stake_pub')
+                    ->unique();
+
+                foreach($registeredStakePub as $stakeAddress) {
+                    dispatch(new SyncCatalystVotingPowersJob(
+                        $snapshot,
+                        $stakeAddress
+                    ));
+                }
+            } catch (\Exception $e) {
+                $this->markAsFailed($m, $e);
+            }
+        });
+    }
+
+    /**
+     * Get the fields available on the action.
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [];
+    }
+}
