7ab212ecb489a33c18abed716d00776621837538
Emmanuel-SE <emmanueltiti370@gmail.com>
2023-09-08 13:04:39 +0300

    LN automating catalyst ledger snapshots sync

---
+++ src/www.lidonation.com/var/www/app/Jobs/SyncCatalystLedgerSnapshots.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Fund;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
+use App\Models\CatalystLedgerSnapshot;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class SyncCatalystLedgerSnapshots implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $nextPage = null;
+
+        do {
+            $response = Http::get('https://archiver.projectcatalyst.io/api/v1/archives' . ($nextPage ? "?next=$nextPage" : ''));
+            $data = $response->json();
+
+            foreach ($data['archives'] as $archive) {
+                $fundTitle = $archive['metadata']['fund'];
+
+                // related fund
+                $fund = Fund::whereRaw("LOWER(REPLACE(title, ' ', '')) = ?", [strtolower($fundTitle)])->first();
+                
+                $existingArchive = CatalystLedgerSnapshot::where('snapshot_id', $archive['id'])->first();
+                if (!$existingArchive instanceof CatalystLedgerSnapshot) {
+                    CatalystLedgerSnapshot::create([
+                        'snapshot_id' => $archive['id'],
+                        'size' => $archive['size'],
+                        'slot' => $archive['metadata']['slot'],
+                        'epoch' => $archive['metadata']['epoch'],
+                        'fund_id' => $fund?->id,
+                        'created_at' => Carbon::parse($archive['metadata']['timestamp']),
+                    ]);
+                }
+            }
+            $nextPage = urlencode($data['next']);
+        } while (!!$nextPage);
+    }
+}
