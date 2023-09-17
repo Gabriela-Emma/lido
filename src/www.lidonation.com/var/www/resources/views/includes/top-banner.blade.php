f31d9b2688160e6b4d54f7032897eb5f5807f3ab
Darlington Kofa <darlington@lidonation.com>
2023-09-17 15:54:40 -0500

    cleanup

--- src/www.lidonation.com/var/www/resources/views/includes/top-banner.blade.php
+++ src/www.lidonation.com/var/www/resources/views/includes/top-banner.blade.php
@@ -1,11 +1,11 @@
-<div class="relative z-20 flex justify-center w-full text-white bg-black global-banner">
-    <a href="{{url('/catalyst-explorer/proposals?l=24&fs[]=113&t=p')}}" class="relative inline-flex items-center gap-2 text-white justify-centerfy-center hover:text-yellow-300 z-100">
-        <span class="py-0.5 md:py-1 text-xs font-bold md:text-md xl:text-md">
-            Catalyst Fund 10 is Live! Start your research.
-        </span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-        </svg>
+{{--<div class="relative z-20 flex justify-center w-full text-white bg-black global-banner">--}}
+{{--    <a href="{{url('/catalyst-explorer/proposals?l=24&fs[]=113&t=p')}}" class="relative inline-flex items-center gap-2 text-white justify-centerfy-center hover:text-yellow-300 z-100">--}}
+{{--        <span class="py-0.5 md:py-1 text-xs font-bold md:text-md xl:text-md">--}}
+{{--            Catalyst Fund 10 is Live! Start your research.--}}
+{{--        </span>--}}
+{{--        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">--}}
+{{--            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />--}}
+{{--        </svg>--}}
 
-    </a>
-</div>
+{{--    </a>--}}
+{{--</div>--}}
