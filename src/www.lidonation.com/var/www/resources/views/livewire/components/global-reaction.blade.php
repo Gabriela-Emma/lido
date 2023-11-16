5d73d2764c442ade34faebb1d39c6722a195d163
pmuigai <pachwalmuiga48@gmail.com>
2023-11-16 15:36:23 +0300

    feature/LN-448-Migrate-post-reaction-to-livewire-component

--- src/www.lidonation.com/var/www/resources/views/livewire/components/global-reaction.blade.php
+++ src/www.lidonation.com/var/www/resources/views/livewire/components/global-reaction.blade.php
@@ -1,12 +1,12 @@
 <div>
-    <div x-data='globalReactions({{ json_encode($post->reactionsCounts) }})'>
+    <div>
         <div class="py-6 border-t border-slate-300">
             <ul class="flex flex-row flex-wrap gap-4 justify-center">
                 @foreach ($post->reactionsCounts as $reaction => $count)
-                    <li @click.prevent="addReaction('{{ $reaction }}', {{ $post->id }})"
+                    <li
                         class="border flex flex-row gap-2 border-slate-600 hover:border-green-500 p-2 rounded-sm text-lg">
-                        <button x-text="'{{ $reaction }}'"></button>
-                        <span x-text="{{ $count }}"></span>
+                        <button  wire:click="addReaction('{{ $reaction }}', {{ $post->id }})" >{{ $reaction }}</button>
+                        <span>{{ $count }}</span>
                     </li>
                 @endforeach
             </ul>
