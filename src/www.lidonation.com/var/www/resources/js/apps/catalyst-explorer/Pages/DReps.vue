31fea1986d7c02e65bc4a1c846f56af417721fed
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2023-11-17 16:31:08 +0300

    LN-460 Update the dReps landing page design

--- src/www.lidonation.com/var/www/resources/js/apps/catalyst-explorer/Pages/DReps.vue
+++ src/www.lidonation.com/var/www/resources/js/apps/catalyst-explorer/Pages/DReps.vue
@@ -1,75 +1,77 @@
 <template>
-    <header-component
-        titleName0="catalyst"
-        titleName1="dReps"
-        subTitle="dReps will play a key role in decision-making for Project Catalyst."
-    />
-    <section class="py-16 bg-primary-20">
-        <div class="container">
-            <div class="grid grid-cols-1 md:grid-cols-2 mx-auto bg-white">
-                <section class="px-6 py-2.5">
-                    <h2 class="mb-6 text-3xl font-bold">
-                        Introducing the Concept of Delegate Representatives
-                        (dReps)
-                    </h2>
+    <section class="bg-white">
+        <header class="py-10">
+            <div class="container">
+                <h1
+                    class="text-3xl font-light leading-8 tracking-tight text-slate-800 sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl pr-8 xl:pr-20 2xl:max-w-7xl"
+                >
+                    <span class="font-bold text-yellow-500">dReps.</span>
+                    <span
+                        >Introducing the Concept of Delegate Representatives
+                        (dReps).</span
+                    >
+                </h1>
+            </div>
+        </header>
 
-                    <div class="divide-y divide-teal-600 space-y-6">
-                        <div>
-                            <p class="text-xl font-bold">
-                                Project Catalyst's Continued Growth and the Need
-                                for Delegation
-                            </p>
+        <div class="bg-white">
+            <div class="space-y-6">
+                <div class="bg-white p-24">
+                    <p
+                        class="text-2xl font-bold text-teal-600 hover:text-teal-800"
+                    >
+                        Project Catalyst's Continued Growth and the Need for
+                        Delegation
+                    </p>
 
-                            <p class="">
-                                The Cardano community's Project Catalyst fund
-                                has experienced remarkable growth since its
-                                inception, with Fund8 receiving nearly 1,000
-                                proposals. This expansion, while positive, poses
-                                a challenge as it increases the community's
-                                responsibility to review and vote on an
-                                ever-growing number of proposals. To address
-                                this challenge and ensure the sustainability of
-                                Project Catalyst, a new system of delegation is
-                                being introduced.
-                            </p>
-                        </div>
-                        <div>
-                            <p class="text-xl font-bold">
-                                Empowering Passive Voters through Delegation
-                            </p>
+                    <p class="text-xl font-light leading-relaxed tracking-wide">
+                        The Cardano community's Project Catalyst fund has
+                        experienced remarkable growth since its inception, with
+                        Fund8 receiving nearly 1,000 proposals. This expansion,
+                        while positive, poses a challenge as it increases the
+                        community's responsibility to review and vote on an
+                        ever-growing number of proposals. To address this
+                        challenge and ensure the sustainability of Project
+                        Catalyst, a new system of delegation is being
+                        introduced.
+                    </p>
+                </div>
+                <div class="bg-primary-20 p-24">
+                    <p
+                        class="text-2xl font-bold text-teal-600 hover:text-teal-800"
+                    >
+                        Empowering Passive Voters through Delegation
+                    </p>
 
-                            <p class="">
-                                Delegation allows ADA holders to entrust their
-                                voting power to one or more Delegate
-                                Representatives (dReps). This system empowers
-                                passive voters, enabling them to have their
-                                voices heard across a broader range of proposals
-                                than they could personally evaluate. dReps will
-                                play a crucial role in Project Catalyst by
-                                voting on the majority of proposals, enhancing
-                                decision-making quality within each Fund.
-                            </p>
-                        </div>
-                        <div>
-                            <p class="text-xl font-bold">
-                                Enhancing Inclusivity and Diversity through dRep
-                                Recruitment
-                            </p>
+                    <p class="text-xl font-light leading-relaxed tracking-wide">
+                        Delegation allows ADA holders to entrust their voting
+                        power to one or more Delegate Representatives (dReps).
+                        This system empowers passive voters, enabling them to
+                        have their voices heard across a broader range of
+                        proposals than they could personally evaluate. dReps
+                        will play a crucial role in Project Catalyst by voting
+                        on the majority of proposals, enhancing decision-making
+                        quality within each Fund.
+                    </p>
+                </div>
+                <div class="bg-white p-24">
+                    <p
+                        class="text-2xl font-bold text-teal-600 hover:text-teal-800"
+                    >
+                        Enhancing Inclusivity and Diversity through dRep
+                        Recruitment
+                    </p>
 
-                            <p class="">
-                                IOG is reopening the application process for the
-                                first dRep cohort, emphasizing inclusivity and
-                                diversity. This move aims to ensure that the
-                                dRep community represents the wider Cardano
-                                community's diverse perspectives and expertise.
-                                As Project Catalyst continues to evolve, the
-                                introduction of dReps marks a significant step
-                                forward in ensuring the fund's sustainability
-                                and effectiveness.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="text-center mt-4">
+                    <p class="text-xl font-light leading-relaxed tracking-wide">
+                        IOG is reopening the application process for the first
+                        dRep cohort, emphasizing inclusivity and diversity. This
+                        move aims to ensure that the dRep community represents
+                        the wider Cardano community's diverse perspectives and
+                        expertise. As Project Catalyst continues to evolve, the
+                        introduction of dReps marks a significant step forward
+                        in ensuring the fund's sustainability and effectiveness.
+                    </p>
+                    <div class="mt-4">
                         <button
                             type="button"
                             class="inline-flex items-center gap-x-2 rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
@@ -89,14 +91,7 @@
                             Sign up to be a dRep
                         </button>
                     </div>
-                </section>
-                <section class="h-full">
-                    <img
-                        class="h-full object-fill"
-                        src="https://media.licdn.com/dms/image/D5610AQFps1GHn9Y2xA/image-shrink_1280/0/1687783676856?e=1700758800&v=beta&t=aH5rEnYu17-Z3QUPAUgKhR_vd8CuwcBD6S6PzU9MMr0"
-                        alt="Cardano dReps Image"
-                    />
-                </section>
+                </div>
             </div>
         </div>
     </section>
