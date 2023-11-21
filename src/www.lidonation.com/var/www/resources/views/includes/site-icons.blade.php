57b90ed8fde3838eb50c038a76e52ef4ee0086fe
Darlington Kofa <darlington@lidonation.com>
2023-11-21 17:44:34 +0000

    resolve files missing from manifest error

--- src/www.lidonation.com/var/www/resources/views/includes/site-icons.blade.php
+++ src/www.lidonation.com/var/www/resources/views/includes/site-icons.blade.php
@@ -1,5 +1,5 @@
+<link rel="icon" type="image/x-icon" href="{{ Vite::image('favicon.ico') }}">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::image('apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::image('favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::image('favicon-16x16.png') }}">
-<link rel="icon" type="image/x-icon" href="{{ Vite::image('favicon.ico') }}">
 
