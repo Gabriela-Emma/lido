e784881b6cb4fc73c140ee698d3a26c9bf0a3019
Darlington Wleh <darlington@raddcreative.com>
2023-11-24 08:52:06 +0000

    Revert "Merge branch 'fix/catalyst-landing-page' into 'dev'"
    This reverts merge request !988

--- src/www.lidonation.com/var/www/app/Inertia/CatalystExplorer/HomeController.php
+++ src/www.lidonation.com/var/www/app/Inertia/CatalystExplorer/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Inertia\CatalystExplorer;
 
-use AllowDynamicProperties;
 use App\Http\Controllers\Controller;
 use App\Models\CatalystExplorer\Fund;
 use Illuminate\Support\Facades\Cache;
@@ -13,11 +12,11 @@ use App\Models\Post;
 use App\Models\Tag;
 use App\Repositories\PostRepository;
 
-#[AllowDynamicProperties] class HomeController extends Controller
+class HomeController extends Controller
 {
-    protected function setTagCloud(): void
+    protected function setTagCloud()
     {
-        $this->wordCloudSet = Cache::remember('catalystDetailsWordCloud', 86400, function () {
+        $this->wordCloudSet = Cache::remember('catalystDetailsWordCloud', DAY_IN_SECONDS, function () {
             $query = DB::select(<<<EOT
         select w.word, SUM(w.num_occurrences) as occurrences
         from proposals t
@@ -36,27 +35,27 @@ use App\Repositories\PostRepository;
         });
     }
 
-    protected function setFundedAverage(): void
+    protected function setFundedAverage()
     {
         $funds = Fund::funds()->withOnly(['proposals'])
             ->withCount([
                 'parent_proposals as proposals_count_amount_requested' => function ($query) {
                     $query->whereNotNull('funded_at')->where('proposals.type', 'proposal');
-                },],
+                }, ],
                 'amount_requested'
             )
             ->withAvg([
                 'parent_proposals as proposals_avg_amount_requested' => function ($query) {
                     $query->whereNotNull('funded_at')->where('proposals.type', 'proposal');
-                },],
+                }, ],
                 'amount_requested'
             )->orderBy('launched_at')
             ->get();
-        $this->fundedAverageSet = $funds->map(fn($p) => [
+        $this->fundedAverageSet = $funds->map(fn ($p) => [
             'label' => $p->title,
             'avg' => $p->proposals_avg_amount_requested,
             'count' => $p->proposals_count_amount_requested,
-        ])->filter(fn($p) => $p['avg'] > 0);
+        ])->filter(fn ($p) => $p['avg'] > 0);
     }
 
     /**
@@ -72,23 +71,24 @@ use App\Repositories\PostRepository;
     public function getCatalystPost(PostRepository $posts)
     {
         $tag = Tag::where('slug', 'project-catalyst')->first();
-        Post::withoutGlobalScopes();
-        $catalystPosts = $posts->inTaxonomies(Tag::class, $tag)
-            ->getQuery()
-            ->latest('published_at')
-            ->limit(4)
-            ->get();
+         $posts->inTaxonomies($tag)->get();
 
-        // Extracting necessary attributes from each post
-        return $catalystPosts->map(function ($post) {
-            return [
-                'title' => $post->title,
-                'subtitle' => $post->subtitle,
-                'image' => $post->getFirstMediaUrl('hero'),
-                'author' => $post->author->name,
-                'author_gravatar' => $post->author->gravatar,
-                'published_at' => $post->published_at->format('M d, Y'),
-            ];
-        });
+         $catalystPosts = $posts->inTaxonomies($tag)->get();
+
+    // Extracting necessary attributes from each post
+    $result = $catalystPosts->map(function ($post) {
+        return [
+            'title'   => $post->title,
+            'subtitle' => $post->subtitle,
+            'image'      => $post->getFirstMediaUrl('hero'),
+            'author'       => $post->author->name, 
+            'author_gravatar' => $post->author->gravatar,
+            'published_at' => $post->published_at->format('M d, Y'),
+        ];
+    });
+
+    // dd($result);
+
+    return $result;
     }
 }
