3647ee1a9535985a8c0d57d879c7744827c336c9
Gabriela-Emma <gabbyoch3@gmail.com>
2023-11-26 16:28:34 +0300

    LN-473 set the library page to eager => true.

--- src/www.lidonation.com/var/www/resources/views/includes/lido-menu-items.blade.php
+++ src/www.lidonation.com/var/www/resources/views/includes/lido-menu-items.blade.php
@@ -1,24 +1,25 @@
+<!-- includes/lido-menu-items.blade.php -->
 <div class="menu-items-wrapper break-inside-avoid mb-8">
     <h3 class="text-base font-bold text-teal-600 w-44 truncate">
         {{$menu?->title}}
     </h3>
     @isset($menu?->items)
         <ul role="list" class="space-y-1 menu-items">
-            @foreach($menu?->items as $menu)
-                @isset($menu->items)
-                    @include('includes.lido-menu-items')
+            @foreach($menu?->items as $menuItem)
+                @isset($menuItem->items)
+                    @include('includes.lido-menu-items', ['menu' => $menuItem])
                 @else
-                    @isset($menu->auth)
+                    @isset($menuItem->auth)
                         @auth()
-                            @include('includes.lido-menu-item')
+                            @include('includes.lido-menu-item', ['menu' => $menuItem])
                         @endauth
                     @else
-                        @isset($menu->guest)
+                        @isset($menuItem->guest)
                             @guest()
-                                @include('includes.lido-menu-item')
+                                @include('includes.lido-menu-item', ['menu' => $menuItem])
                             @endguest
                         @else
-                            @include('includes.lido-menu-item')
+                            @include('includes.lido-menu-item', ['menu' => $menuItem])
                         @endisset
                     @endisset
                 @endisset
@@ -26,4 +27,3 @@
         </ul>
     @endisset
 </div>
-
