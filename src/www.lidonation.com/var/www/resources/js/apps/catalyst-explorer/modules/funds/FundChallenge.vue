2ac2f1f4f039fdf4ab0046c9e4fd01c4f433768a
mteddy <michaelteddy009@gmail.com>
2023-12-01 10:20:57 +0300

    feature/LN-488-fund-card-update-clean-up

--- src/www.lidonation.com/var/www/resources/js/apps/catalyst-explorer/modules/funds/FundChallenge.vue
+++ src/www.lidonation.com/var/www/resources/js/apps/catalyst-explorer/modules/funds/FundChallenge.vue
@@ -1,58 +1,41 @@
 <template>
     <div
-        class="flex flex-col divide-y rounded items-start justify-center text-center rounded-md bg-white xl:text-left">
-        <!-- thumbnail or gravatar -->
-        <a v-bind:href="fund.link" class="w-full h-64">
-            <img class="w-full h-full" v-bind:src="fund.thumbnail_url ?? fund.gravatar"
-                v-bind:alt="fund.title" />
+        class="relative flex flex-col overflow-hidden bg-white border border-gray-200 rounded-lg group hover:cursor-pointer">
+        <a :href="fund.link" class="bg-gray-200 aspect-w-2 aspect-h-2 group-hover:opacity-75 sm:aspect-none sm:h-60">
+            <img :alt="fund.title" :src="fund.thumbnail_url ?? fund.hero_url"
+                class="object-cover object-center w-full h-full sm:w-full sm:h-full">
         </a>
-
-        <!-- content and budget -->
-        <div class="flex flex-col w-full h-auto divide-y xl:px-8 bg-white">
-            <div class="pt-8 flex flex-col items-start justify-between w-full space-y-6 xl:space-y-10">
-                <div class="items-end w-full space-y-2 xl:flex xl:items-center xl:justify-between">
-                    <div class="w-full space-y-1 text-lg font-medium leading-6">
-                        <h2 class="mb-2">
-                            <a v-bind:href="fund.link" class="text-left text-gray-800 hover:text-teal-700">
-                                {{ fund.label }}
-                            </a>
-                        </h2>
-                        <div class="flex flex-col items-start justify-between w-full gap-2">
-                            <div class="h-48 text-lg font-medium text-left text-gray-500 min-h-1/2 overflow-hidden">
-                                {{ fund.content.length > 150 ? fund.content.slice(150) + ' ...' : fund.content }}
-                            </div>
-                            
-                            <div class="flex flex-row justify-center pt-4 gap-2">
-                                <span class="text-lg font-medium text-gray-500">Budget:</span>
-                                <span class="text-lg font-semibold text-gray-600">
-                                    {{ $filters.shortNumber(fund.amount, 2) }} ₳
-                                </span>
-                            </div>
+        <div class="flex flex-col flex-1 p-4 space-y-2">
+            <h2>
+                <a v-bind:href="fund.link" class="text-left text-gray-800 hover:text-teal-700">
+                    {{ fund.label }}
+                </a>
+            </h2>
+            <p class="text-sm text-gray-500 h-auto">{{ fund.content.length > 150 ? fund.content.slice(0, 300) + ' ...' :
+                fund.content }}</p>
+            <div class="flex flex-col justify-end flex-1 divide-y">
+                <p class="text-lg font-medium text-gray-900">
+                    Budget: <span class="font-bold">{{ $filters.shortNumber(fund.amount, 2) }} ₳</span>
+                </p>
+                <div class="w-full grid mt-4 grid-cols-2 -mt-px text-sm divide-x xl:text-sm 2xl:text-md">
+                    <div class="flex items-center justify-center flex-1 gap-2 p-2">
+                        <div class="text-sm">
+                            {{ $t("Proposals") }}:
+                        </div>
+                        <div class="font-semibold">
+                            {{ fund.proposals_count }}
                         </div>
                     </div>
-                </div> 
-            </div>
-        </div>
 
-        <!-- proposed and approved, very bottom -->   
-        <div class="w-full mt-6 grid grid-cols-2 -mt-px text-sm divide-x xl:text-sm 2xl:text-md">
-            <div class="flex items-center justify-center flex-1 gap-2 p-2">
-                <div class="text-sm">
-                    {{  $t("Proposals") }}:
-                </div>
-                <div class="font-semibold">
-                    {{ fund.proposals_count }}
-                </div>
-            </div>
-
-            <div class="flex flex-1 -ml-px">
-                <div
-                    class="flex items-center justify-center flex-1 gap-2 p-2">
-                    <div class="text-sm">
-                        {{  $t("Approved") }}:
-                    </div>
-                    <div class="font-semibold">
-                        {{ fund.funded_proposals }}
+                    <div class="flex flex-1 -ml-px">
+                        <div class="flex items-center justify-center flex-1 gap-2 p-2">
+                            <div class="text-sm">
+                                {{ $t("Approved") }}:
+                            </div>
+                            <div class="font-semibold">
+                                {{ fund.funded_proposals }}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
