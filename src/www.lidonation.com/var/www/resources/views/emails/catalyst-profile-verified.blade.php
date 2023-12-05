8546575dde5556f8ccceeae71933204b1747f4a8
Darlington Kofa <darlington@lidonation.com>
2023-12-05 19:45:29 +0000

    cleanup catalyst user email verification failing

--- src/www.lidonation.com/var/www/resources/views/emails/catalyst-profile-verified.blade.php
+++ src/www.lidonation.com/var/www/resources/views/emails/catalyst-profile-verified.blade.php
@@ -5,10 +5,10 @@
 
 Set a password to start managing your profile!
 
-After you've set a password, you may manage all proposals you are the primary author of via your [Proposals Dashboard]({{localizeRoute('catalystExplorer.myProposals')}}).
+After you've set a password, you may manage all proposals you are the primary author of via your [Proposals Dashboard]({{localizeRoute('catalyst-explorer.myProposals')}}).
 
 If you would like other members of your team to be able to manage your proposal, they will need to claim their profile,
-then you will be able to assign permissions from the [Groups Dashboard]({{localizeRoute('catalystExplorer.myGroups')}}).
+then you will be able to assign permissions from the [Groups Dashboard]({{localizeRoute('catalyst-explorer.myGroups')}}).
 
 If you have any questions or need any support email hello@lidonation.com or reach out on twitter @lidonation.
 
