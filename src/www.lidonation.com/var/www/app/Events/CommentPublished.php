d4ed150c75caecb994a5f4005b0956c97677c86b
Darlington Kofa <darlington@lidonation.com>
2023-12-05 18:06:41 +0000

    fix events classes missing

---
+++ src/www.lidonation.com/var/www/app/Events/CommentPublished.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\CatalystExplorer\Assessment;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CommentPublished
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public Assessment $comment)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     */
+    public function broadcastOn(): Channel|PrivateChannel|array
+    {
+        return new PrivateChannel('app');
+    }
+}
