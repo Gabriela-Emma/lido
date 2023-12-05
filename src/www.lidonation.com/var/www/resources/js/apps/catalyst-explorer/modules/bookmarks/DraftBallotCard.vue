35cf16f493e908e21748be780d43c664891e1b1b
Darlington Kofa <darlington@lidonation.com>
2023-12-05 12:57:46 +0000

    cleanup bookmarks

--- src/www.lidonation.com/var/www/resources/js/apps/catalyst-explorer/modules/bookmarks/DraftBallotCard.vue
+++ src/www.lidonation.com/var/www/resources/js/apps/catalyst-explorer/modules/bookmarks/DraftBallotCard.vue
@@ -1,24 +1,42 @@
 <template>
     <div :style="{ backgroundColor: draftBallot?.color }"
         class="relative flex flex-col justify-center object-cover w-full h-full text-white shadow-md hover:shadow-xl rounded-l-xl rounded-r-xs">
-        <a :href="draftBallot?.link">
-            <div class="relative flex justify-end my-auto">
-                <h2
-                    class="box-border inline w-4/5 px-3 py-4 my-auto text-xl font-bold tracking-tight bg-white rounded-l-lg sm:text-2xl box-decoration-cloe text-slate-800 -right-6">
-                    {{ draftBallot?.title }}
-                </h2>
-            </div>
-            <div class="absolute flex justify-end w-full gap-2 px-3 divide-x divide-slate-800 bottom-3"
-                v-if="draftBallot?.items_count > 0">
-                <div class="inline-flex items-center rounded-sm py-0.5 pl-2.5 pr-1 text-sm font-medium text-black">
-                    {{ $t("Items") }}
-                    <span
-                        class="ml-0.5 inline-flex flex-shrink-0 items-center justify-center rounded-full font-bold text-black focus:outline-none">
-                        {{ draftBallot?.items_count }}
-                    </span>
+        <div class="relative flex justify-end my-auto">
+            <div class="box-border inline w-4/5 px-3 py-4 my-auto bg-white rounded-l-lg  box-decoration-cloe  -right-6">
+                <div class="text-xl sm:text-2xl font-bold tracking-tight text-slate-800 flex flex-col items-start">
+                    <small
+                        class="inline-flex items-center rounded-sm py-0.5 pr-1 text-xs font-medium">
+                        {{ $t("Items") }}
+                        <span
+                            class="ml-0.5 inline-flex flex-shrink-0 items-center justify-center rounded-full font-bold focus:outline-none">
+                            {{ draftBallot?.items_count }}
+                        </span>
+                    </small>
+                    <span class="inline-flex line-clamp-3">{{ draftBallot?.title }}</span>
+                </div>
+
+                <div class="flex gap-2 mt-2">
+                    <Link as="button" type="button" :href="route('catalyst-explorer.draftBallot.edit', {draftBallot: draftBallot?.hash})"
+                          class="hover:text-slate-400 text-xs font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-800">
+                        {{ $t("Edit") }}
+                    </Link>
+                    <Link as="button" type="button" :href="route('catalyst-explorer.draftBallot.view', {draftBallot: draftBallot?.hash})"
+                          class="hover:text-slate-400 text-xs font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-800">
+                        {{ $t("View") }}
+                    </Link>
                 </div>
             </div>
-        </a>
+<!--            <div class="absolute flex justify-end w-full gap-2 px-3 divide-x divide-slate-800 bottom-3"-->
+<!--                v-if="draftBallot?.items_count > 0">-->
+<!--                <div class="inline-flex items-center rounded-sm py-0.5 pl-2.5 pr-1 text-sm font-medium text-black">-->
+<!--                    {{ $t("Items") }}-->
+<!--                    <span-->
+<!--                        class="ml-0.5 inline-flex flex-shrink-0 items-center justify-center rounded-full font-bold text-black focus:outline-none">-->
+<!--                        {{ draftBallot?.items_count }}-->
+<!--                    </span>-->
+<!--                </div>-->
+<!--            </div>-->
+        </div>
     </div>
 </template>
 
