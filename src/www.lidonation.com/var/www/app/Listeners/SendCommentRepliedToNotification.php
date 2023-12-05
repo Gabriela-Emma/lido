63fd51afcdb5efab8fca553290d5014abe4eb032
Darlington Kofa <darlington@lidonation.com>
2023-12-05 19:07:12 +0000

    restore missing listeners

---
+++ src/www.lidonation.com/var/www/app/Listeners/SendCommentRepliedToNotification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CommentRepliedTo;
+use App\Mail\CommentRepliedToMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendCommentRepliedToNotification implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @return void
+     */
+    public function handle(CommentRepliedTo $event)
+    {
+        if (isset($event->comment->parent->email)) {
+            Mail::to($event->comment->parent->email)
+                ->send(new CommentRepliedToMail($event->comment));
+        }
+    }
+}
